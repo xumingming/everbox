a6dbfa926aeed418c2c6375a0d15db0c048aa65a
James Xu <xumingming64398966@gmail.com>
2012-09-03 20:41:36 +0800

    add tests for list-cache-notes & handle-notes-creation

--- test/everbox/core_test.clj
+++ test/everbox/core_test.clj
@@ -19,6 +19,35 @@
       (let [nb (@notebooks "notebook1")
             updated-note ((:notes nb) "note1")]
         (is (= 2000 (:updated-at updated-note)))
-        (is (= "updated-content" (:content updated-note))))
+        (is (= "updated-content" (:content updated-note)))))))
+
+(deftest test-handle-notes-creation
+  (let [note1 {:guid "note1"
+               :title "note-title"
+               :content "note-content"
+               :updated-at 1000}
+        notebook1 {:guid "notebook1"
+                   :name "notebook-name"
+                   :notes {}}
+        notebooks (atom {"notebook1" notebook1})]
+    (with-redefs [compute-need-to-create-notes (fn [& _] ["note1"])
+                  create-note (fn [& _] note1)
+                  get-local-note-content (fn [& _] "local-content")]
+      (handle-notes-creation notebooks notebook1)
+      (let [nb (@notebooks "notebook1")
+            created-note ((:notes nb) "note1")]
+        (is (= 1 (count (:notes nb)))))
       (println @notebooks))))
 
+
+(deftest test-list-cache-notes
+  (let [note1 {:guid "note1"
+               :title "note-title"
+               :content "note-content"
+               :updated-at 1000}
+        notebook1 {:guid "notebook1"
+                   :name "notebook-name"
+                   :notes {"note1" note1}}
+        notebooks (atom {"notebook1" notebook1})
+        cached-notes (list-cache-notes notebooks "notebook1")]
+    (is (= ["note-title"] (vec cached-notes)))))
